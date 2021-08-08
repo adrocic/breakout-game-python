220e52c1d8583de45bb9ad5e34e87dcb373f47e4
Anel Drocic <anel.drocic@gmail.com>
2021-08-07 23:58:26 -0500

    updated gameObjectsHandler and main.py with static methods and fields to separate logic out and make files more readable

--- gameObjectsHandler.py
+++ gameObjectsHandler.py
@@ -4,15 +4,24 @@ from brick import Brick
 from constants import PADDLE_START_X, PADDLE_START_Y
 
 paddle = Paddle(PADDLE_START_X, PADDLE_START_Y, 700, 120, 20, (245, 227, 255))
-startingBall = Ball(1, 1, 380, 680, 10, 10, (245, 227, 255))
-allBalls = [startingBall]
+allBalls = []
 bricks = []
 
 def createBricksArray():
+    bricks.clear()
     for i in range(6):
         for j in range(10):
             bricks.append(Brick(10 + j * 79, 50 + i * 35, 70, 25, (120, 205, 250)))
     return bricks
+
+def createBallsArray():
+    allBalls.clear()
+    startingBall = Ball(1, 1, 380, 680, 10, 10, (245, 227, 255))
+    allBalls.append(startingBall)
+    return allBalls
+
+def addBallToGame():
+    allBalls.append(Ball(1, 1, 380, 680, 10, 10, (245, 227, 255)))
 
 def drawAllBalls(window):
     for ball in allBalls:
@@ -23,8 +32,10 @@ def drawAllBricks(window):
     for brick in bricks:
         brick.draw(window)
 
+
 def drawPaddle(window):
     paddle.draw(window)
+
 
 def drawAllGameItems(window):
     drawPaddle(window)
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from gameObjectsHandler import drawAllGameItems, createBricksArray
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, REFRESH_RATE
+from gameObjectsHandler import paddle, drawAllGameItems, createBricksArray, createBallsArray, addBallToGame
 
 # pygame stuff
 pygame.init()
 pygame.display.set_caption("Breakout")
 background = pygame.image.load('background.png')
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+font = pygame.font.SysFont('comicsans', 50)
 
 # game audio
 brickHitSound = pygame.mixer.Sound("bullet.wav")
@@ -15,6 +16,10 @@ bounceSound.set_volume(.2)
 
 # initial game items
 bricks = createBricksArray()
+allBalls = createBallsArray()
+winMessage = winMessage = font.render("You Won!", 1, (255, 255, 255))
+loseMessage = font.render("You Lost!", 1, (255, 255, 255))
+playAgainMessage = font.render("Press Space to Play Again", 1, (255, 255, 255))
 
 # handle game window redraws
 gameover = False
@@ -22,33 +27,24 @@ def redrawGameWindow():
     window.blit(background, (0, 0))
     drawAllGameItems(window)
 
-    font = pygame.font.SysFont('comicsans', 50)
-
     if gameover:
-        if len(bricks) == 0:
-            winMessage = font.render("Congrats!", 1, (255, 255, 255))
-        else:
-            loseMessage = font.render("That's rough!", 1, (255, 255, 255))
-        window.blit(resText,
-                    ((SCREEN_WIDTH // 2 - resText.get_width() // 2), screen_height // 2 - resText.get_height() // 2))
-        playAgainText = font.render("Press Space to Play Again", 1, (255, 255, 255))
-        window.blit(playAgainText, ((screen_width // 2 - playAgainText.get_width() // 2), screen_height // 2 + 30))
+        window.blit(winMessage if len(bricks) else loseMessage, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+        window.blit(playAgainMessage, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
-    pygame.display.update()
-
+    pygame.display.flip()
 
 
 clock = pygame.time.Clock()
 running = True
 while running:
-    clock.tick(100)
+    clock.tick(REFRESH_RATE)
     if not gameover:
         for ball in allBalls:
             ball.moveBall()
         if pygame.mouse.get_pos()[0] - paddle.width // 2 < 0:
             paddle.xPos = 0
-        elif pygame.mouse.get_pos()[0] + paddle.width // 2 > screen_width:
-            paddle.xPos = screen_width - paddle.width
+        elif pygame.mouse.get_pos()[0] + paddle.width // 2 > SCREEN_WIDTH:
+            paddle.xPos = SCREEN_WIDTH - paddle.width
         else:
             paddle.xPos = pygame.mouse.get_pos()[0] - paddle.width // 2
 
@@ -60,7 +56,7 @@ while running:
                     ball.yPos = paddle.yPos - ball.height - 1
                     bounceSound.play()
 
-            if ball.xPos + ball.width >= screen_width:
+            if ball.xPos + ball.width >= SCREEN_WIDTH:
                 bounceSound.play()
                 ball.velocityX *= -1
             if ball.xPos < 0:
@@ -70,7 +66,7 @@ while running:
                 bounceSound.play()
                 ball.velocityY *= -1
 
-            if ball.yPos > screen_height:
+            if ball.yPos > SCREEN_HEIGHT:
                 allBalls.pop(allBalls.index(ball))
 
         for brick in bricks:
@@ -81,7 +77,7 @@ while running:
                             brick.yPos <= ball.yPos <= brick.yPos + brick.height) or brick.yPos <= ball.yPos + ball.height <= brick.yPos + brick.height:
                         brick.exists = False
                         if brick.containsBall:
-                            allBalls.append(Ball(3, 3, brick.xPos, brick.yPos, 20, 20, (255, 255, 255)))
+                            addBallToGame()
                         ball.velocityY *= -1
                         brickHitSound.play()
                         break
@@ -101,14 +97,8 @@ while running:
         if keys[pygame.K_SPACE]:
             gameover = False
             won = False
-            ball = Ball(3, 3, screen_width / 2 - 10, screen_height - 400, 20, 20, (255, 255, 255))
-            if len(allBalls) == 0:
-                allBalls.append(ball)
-
-            bricks.clear()
-            for i in range(6):
-                for j in range(10):
-                    bricks.append(Brick(10 + j * 79, 50 + i * 35, 70, 25, (120, 205, 250)))
+            createBallsArray()
+            createBricksArray()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
